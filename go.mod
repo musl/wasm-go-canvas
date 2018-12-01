a499242732d4efb1bb5f850d0447d8c533f3f4d1
Mike Hix <mhix@newrelic.com>
2018-12-01 13:24:06 -0800

    Make a dynamically animated SVG from go with WASM

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/musl/wasm-go-canvas
 
-require github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
+	github.com/lucasb-eyer/go-colorful v0.0.0-20181028223441-12d3b2882a08
+)
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 	"syscall/js"
 	"time"
 
-	"github.com/alecthomas/template"
+	template "github.com/alecthomas/template"
+	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 const (
@@ -28,46 +30,88 @@ var (
 	Revision = ``
 )
 
-func draw(args []js.Value) {
-	if len(args) != 2 {
-		log.Printf("draw needs 2 arguments, an id to draw to, and a number of frames.")
-		return
+// Element is
+type Element interface {
+	Update()
+	String() string
+}
+
+// Context is everything we need to animate our SVG canvas.
+type Context struct {
+	Canvas   js.Value
+	Document js.Value
+	Elements []Element
+	FPS      float64
+}
+
+// NewContext returns a new Context struct
+func NewContext(fps float64, selector string) *Context {
+	doc := js.Global().Get(`document`)
+	c := &Context{
+		FPS:      fps,
+		Document: doc,
+		Canvas:   doc.Call(`querySelector`, selector),
 	}
 
-	doc := js.Global().Get("document")
-	id := args[0].String()
-	frames := args[1].Int()
-	svg := doc.Call("getElementById", id)
+	return c
+}
 
-	// 1000 / 60 = 16.666...
-	step := math.Pi / 120.0
-	frameDelay := 16666 * time.Nanosecond
-	tmplString := `<circle cx="{{.X}}" cy="{{.Y}}" r="25" stroke="#ddd" stroke-width="1" fill="#def" />`
-	tmplData := struct {
-		X int
-		Y int
-	}{}
+func setup(c *Context) {
+}
+
+// Spinner is.
+type Spinner struct {
+	A           float64
+	Fill        string
+	I           float64
+	IMax        float64
+	O           float64
+	Opacity     float64
+	P           float64
+	R           float64
+	Step        float64
+	Stroke      string
+	StrokeWidth float64
+	T           float64
+	X           int
+	Y           int
+}
+
+// Update moves a Spinner.
+func (s *Spinner) Update() {
+	s.T += s.Step
+	s.P = ((4.0 * math.Pi / s.IMax) * s.I) + math.Sin((s.I/s.IMax)*s.T)
+	s.X = int(s.O + s.A*math.Cos(s.T+s.P))
+	s.Y = int(s.O + s.A*math.Sin(s.T+s.P))
+}
+
+func (s *Spinner) String() string {
+	tmplString := `<circle cx="{{.X}}" cy="{{.Y}}" r="{{.R}}" opacity="{{.Opacity}}" stroke="{{.Stroke}}" stroke-width="{{.StrokeWidth}}" fill="{{.Fill}}" />`
 	tmpl, err := template.New("svg").Parse(tmplString)
 	if err != nil {
 		log.Printf("Unable to compile the SVG template: %s", err.Error())
-		return
+		return ""
 	}
 	var tmplWriter strings.Builder
 
-	for i := 0; i < frames; i++ {
-		tmplData.X = int(250 + 200*math.Cos(float64(i)*step))
-		tmplData.Y = int(250 + 200*math.Sin(float64(i)*step))
+	err = tmpl.Execute(&tmplWriter, s)
+	if err != nil {
+		log.Printf("Unable to render the SVG template: %s", err.Error())
+		return ""
+	}
+	return tmplWriter.String()
+}
 
-		err = tmpl.Execute(&tmplWriter, tmplData)
-		if err != nil {
-			log.Printf("Unable to render the SVG template: %s", err.Error())
-			return
+func loop(c *Context) {
+	delay := time.Duration(float64(time.Second) / c.FPS)
+	for {
+		svg := make([]string, len(c.Elements))
+		for i := range c.Elements {
+			c.Elements[i].Update()
+			svg[i] = c.Elements[i].String()
 		}
-
-		svg.Set("innerHTML", tmplWriter.String())
-		tmplWriter.Reset()
-
-		time.Sleep(frameDelay)
+		c.Canvas.Set("innerHTML", strings.Join(svg, ``))
+		time.Sleep(delay)
 	}
 }
 
@@ -78,12 +122,30 @@ func main() {
 
 	log.Printf("%s version: %s revision: %s", AppName, Version, Revision)
 
-	//
-	// Setup Callbacks
-	//
-	cb := js.NewCallback(draw)
-	defer cb.Release()
-	js.Global().Set("draw", cb)
+	c := NewContext(60, `svg#canvas`)
+	w, err := strconv.Atoi(c.Canvas.Call(`getAttribute`, `width`).String())
+	if err != nil {
+		return
+	}
+
+	iMax := 16
+	for i := 0; i < iMax; i++ {
+		c.Elements = append(c.Elements,
+			&Spinner{
+				I:           float64(i),
+				IMax:        float64(iMax),
+				A:           10 + (float64(w)/3)*(float64(i)/float64(iMax)),
+				O:           (float64(w) / 2.0),
+				R:           16.0 + (float64(w)/32.0)*(float64(i)/float64(iMax)),
+				Opacity:     0.5,
+				Step:        math.Pi / 60.0,
+				StrokeWidth: 1.0,
+				Stroke:      colorful.Hsv(360*(float64(i)/float64(iMax)), 1.0, 1.0).Hex(),
+				Fill:        colorful.Hsv(360*(float64(i)/float64(iMax)), 0.2, 1.0).Hex(),
+			},
+		)
+	}
+	go loop(c)
 
 	//
 	// Handle Unload
